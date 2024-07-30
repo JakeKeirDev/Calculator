57050dd5e9f69d6ef72a2b5fc264a2fde56ff65d
Jake Keir <jakekeirdev@gmail.com>
2024-07-30 19:54:56 +1000

    Operate Function
    Implemented operate function to call add, subtract, divide, or multiply.
    Changed numerical type checking to operate function instead of checking after parsed into each function.

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
+//Global variables for each number and the operator.
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = '';
+
 
 //Functions for addition, subtraction, multiplication and division.
-//Each checks if both parsed variables are numerical.
 const add = function(x, y) {
-    if (typeof(x) != "number" || typeof(y) != "number") {
-        return ("ERROR.")
-    }
+    
     return(x + y);
 }
 
 const subtract = function(x, y) {
-    if (typeof(x) != "number" || typeof(y) != "number") {
-        return ("ERROR.")
-    }
     return(x - y);
 }
 
 const divide = function(x, y) {
-    if (typeof(x) != "number" || typeof(y) != "number") {
-        return ("ERROR.")
-    }
     return(x / y);
 }
 
 const multiply = function(x, y) {
-    if (typeof(x) != "number" || typeof(y) != "number") {
-        return ("ERROR.")
-    }
     return(x * y);
+}
+
+
+//Operate function initially checks if both parsed variables are numerical.
+//Calls add, subtract, divide, or multiply depending operator parsed.
+const operate = function(firstNumber, operator, secondNumber) {
+    if (typeof(firstNumber) != "number" || typeof(secondNumber) != "number") {
+        return ("Error - non numerical input.")
+    }
+    
+    if (operator == '+') { add(firstNumber, secondNumber)}
+    else if (operator == '-') { subtract(firstNumber, secondNumber)}
+    else if (operator == '/') { divide(firstNumber, secondNumber)}
+    else if (operator == '*') { multiply(firstNumber, secondNumber)}
+    else {return ("Error - Operator unrecognised.")}
 }
